93928481d1b55f20e71360582403c7823e226eae
Alejandro Martin Achinelli <alejandromartin.achinelli@gmail.com>
2025-12-30 13:21:51 -0300

    feat: dbcontext

---
+++ src/Atlas.Modules.Identity/DbContext/IdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Atlas.Modules.Identity.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Atlas.Modules.Identity.DbContext;
+
+public class IdentityDbContext : DbContext
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Aplica configuraciones (las creamos en el paso 3)
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+    }
+}
